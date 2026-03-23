ce61d2e448a9eae6230185474f7b47bc27e57a58
Abd AL Rahman Miassar <abodimiassar@gmail.com>
2026-03-24 02:17:38 +0300

    Reapply "Add new lines to feature branch"
    This reverts commit cf6ccb3817607e2c6d4ab04871e881114cebfa17.

--- Program.cs
+++ Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Welcome to C# programming.");
 Console.WriteLine("This is my first C# project.");
 Console.WriteLine("I am excited to learn more about C# and build amazing applications!");
 
+Console.WriteLine("This line is from the new branch!");
+Console.WriteLine("This line is from the main branch!");
